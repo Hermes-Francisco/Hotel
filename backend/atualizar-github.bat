02854460267d511b4f0706578349900bc98cd092
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-04-27 20:44:18 -0300

    testando o arquivo 'atualizar-git.bat' - 27/04/2020

--- backend/atualizar-github.bat
+++ backend/atualizar-github.bat
@@ -2,7 +2,23 @@
 cls
 title atualizando projeto Hotel no meu gitHub
 echo escreva a mensagem do commit.
-git add .
 set /p mensagem=
+cls
+echo confirmar atualizacao com a mensagem "%mensagem%"? (S/N)
+set /p sn=
+if %sn% == s goto sim
+goto nao
+:sim
+cls
+title atualizando...
+git add .
 git commit -m "%mensagem% - %date%"
 git push
+title ultima atualizacao %date% as %time%.
+:nao
+cls
+title envio cancelado.
+echo envio cancelado.
+pause
+cls
+title .
