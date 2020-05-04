ecf7f3a718f1ea0ff497f796766a161272d3e8f5
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-05-04 19:40:52 -0300

    find - 04/05/2020

--- backend/src/controllers/HotelController.js
+++ backend/src/controllers/HotelController.js
@@ -7,7 +7,7 @@ class HotelController{
 	
 		const { municipio } = req.params;
 		
-		let Hoteis = await Hotel.findMany({ municipio })
+		let Hoteis = await Hotel.find({ municipio })
 		
 		return res.json({Hoteis})
 	}
