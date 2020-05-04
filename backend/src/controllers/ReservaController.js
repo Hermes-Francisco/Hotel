80a5092436618766842e7e15a0e88da95d3482bc
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-05-04 20:33:32 -0300

    reserva - 04/05/2020

--- backend/src/controllers/ReservaController.js
+++ backend/src/controllers/ReservaController.js
@@ -1,4 +1,6 @@
 import Reserva from '../models/Reserva';
+import Usuario from '../models/Usuario';
+import Hotel from '../models/Hotel';
 
 class ReservaController{
 	
@@ -9,8 +11,15 @@ class ReservaController{
 	}
 	
 	async store(req, res){
-		const { usuario_id, dataInicial, dataFinal, qtdeHospedes } = req.body;
+		const { dataInicial, dataFinal, qtdeHospedes } = req.body;
 		const { hotel_id } = req.params;
+		const { usuario_id } = req.headers;
+		
+		let hotel = Hotel.findOne({ _id: hotel_id });
+		let usuario = Usuario.findOne({ _id: usuario_id });
+		
+		if(!hotel)return res.status(404).json({mensagem: 'hotel não encontrado'});
+		if(!usuario)return res.status(404).json({mensagem: 'usuario não encontrado'});
 		
 		let reserva = await Reserva.create({ responsavel: usuario_id, hotel: hotel_id, dataInicial, dataFinal, qtdeHospedes });
 		
--- backend/src/models/Reserva.js
+++ backend/src/models/Reserva.js
@@ -1,8 +1,8 @@
 import { Schema, model } from 'mongoose';
 
 const ReservaSchema = new Schema({
-	responsavel: { type: Screma.Types.ObjectId, ref: 'Usuario' },
-	hotel: { type: Screma.Types.ObjectId, ref: 'Hotel' },
+	responsavel: { type: Schema.Types.ObjectId, ref: 'Usuario' },
+	hotel: { type: Schema.Types.ObjectId, ref: 'Hotel' },
 	dataInicial: String,
 	dataFinal: String,
 	qtdeHospedes: Number,
